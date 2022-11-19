8c8a0db2da515ce66bfe168e88aa7465a90d465f
Nathan Ferreira Silva <nathanfs710@gmail.com>
2022-11-19 20:17:28 -0300

    Consertando a navegação

--- app/src/main/java/com/nathan/wswork/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/nathan/wswork/ui/details/DetailsFragment.kt
@@ -1,8 +1,32 @@
 package com.nathan.wswork.ui.details
 
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.nathan.wswork.R
+import com.nathan.wswork.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_main, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpClickListeners()
+    }
+
+    private fun setUpClickListeners() {
+        TODO("Not yet implemented")
+    }
 
 }
--- app/src/main/java/com/nathan/wswork/ui/main/MainFragment.kt
+++ app/src/main/java/com/nathan/wswork/ui/main/MainFragment.kt
@@ -1,18 +1,37 @@
 package com.nathan.wswork.ui.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.nathan.wswork.R
+import com.nathan.wswork.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_main, container,false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.button).setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_detailsFragment)
+
+        setUpClickListeners()
+    }
+
+    private fun setUpClickListeners() {
+        with(binding) {
+            button.setOnClickListener {
+                findNavController().navigate(R.id.action_mainFragment_to_detailsFragment)
+            }
         }
+        TODO("Not yet implemented")
     }
 }
